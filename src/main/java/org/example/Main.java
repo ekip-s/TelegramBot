84634abccef6604df8918f879c84fee5843637d6
Egor <b9955047856@gmail.com>
2023-02-05 18:51:13 +0300

    feat: загрузка фото с url nasa;

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,47 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello programming");
+
+    private static final String URI = "https://api.nasa.gov/planetary/" +
+                "apod?api_key=eqeuCxifYsF27kBL1QN1N4BxoEue9sluGEf6kDIj";
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        CloseableHttpResponse response = httpClient.execute(new HttpGet(URI));
+
+        NasaObject nasaObject = mapper.readValue(response.getEntity().getContent(), NasaObject.class);
+        System.out.println(nasaObject);
+
+        CloseableHttpResponse pictureResponse = httpClient.execute(new HttpGet(nasaObject.getUrl()));
+
+        String[] arr = nasaObject.getUrl().split("/");
+        String file = arr[6];
+
+        HttpEntity entity = pictureResponse.getEntity();
+        if (entity != null) {
+            FileOutputStream fos = new FileOutputStream(file);
+            entity.writeTo(fos);
+            fos.close();
+        }
     }
 }
